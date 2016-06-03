88d7844a0ad73464684017e2c15707b185d8661e
Adriana Galvez <vntgh@yahoo.com>
2016-06-03 14:21:03 -0600

    Module 5 Complete

--- script.js
+++ script.js
@@ -5,14 +5,18 @@ document.addEventListener("DOMContentLoaded", function(){
     if (request.readyState === 4 && request.status < 400){
       var array = JSON.parse(request.responseText).results;
       var list = [];
+      var check = [];
       for (var album in array){
         list.push(array[album]["cover_art"]);
       }
-      for (var i = 0; i < 3; i++) {
+      for (var i = 0; i < 10; i++) {
         var element = list[Math.floor(Math.random()*list.length)];
+        if ((check.indexOf(element) == -1) && (check.length < 3)){
+        check.push(element);
         var img = document.createElement("img");
         img.src = "images/" + element;
         images.appendChild(img);
+        }
       }
     }
   };
--- script2.js
+++ script2.js
@@ -1,4 +1,9 @@
-var scroll = document.getElementById("scroll")
+var scroll = document.getElementById("scroll");
+var select = document.getElementById("list");
+var clear = document.getElementById("clear");
+var submit = document.getElementById("submit");
+var collection = [];
+
 document.addEventListener("DOMContentLoaded", function(){
   var request = new XMLHttpRequest();
   request.onreadystatechange = function(){
@@ -16,8 +21,39 @@ document.addEventListener("DOMContentLoaded", function(){
         }
         scroll.appendChild(img);
       }
+      var images = document.getElementsByTagName("img");
+      for (var i = 0; i < images.length; i++) {
+        images[i].addEventListener("click", function(event){
+            var target = event.target;
+            for (var al in array){
+              if ((array[al]["id"] == target.id) && (collection.indexOf(array[al].title) == -1)){
+                var newl = document.createElement("h5");
+                newl.innerHTML = array[al].artist + ": " + array[al].title;
+                select.appendChild(newl);
+                collection.push(array[al].title);
+              }
+          }
+        });
+
+      }
     }
   };
   request.open("GET", "https://lit-fortress-6467.herokuapp.com/object");
   request.send();
+});
+
+clear.addEventListener("click", function(){
+  select.innerHTML = "";
+  collection = [];
+});
+
+submit.addEventListener("click", function(){
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function(){
+    if (request.readyState === 4 && request.status < 400){
+        console.log(request.responseText);
+    }
+  };
+    request.open("POST", "https://lit-fortress-6467.herokuapp.com/post");
+    request.send(collection);
 });
