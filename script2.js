1c9ea7bd08be9ff68da6e0339a467c072d7bf01e
Adriana Galvez <vntgh@yahoo.com>
2016-06-07 14:28:22 -0600

    Refactor WIP

--- script2.js
+++ script2.js
@@ -7,7 +7,22 @@ var search = document.getElementById("search");
 var input = document.getElementById("input");
 var opt;
 
-search.addEventListener("click", function start(){
+function getAlbums(opt, callback) {
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function () {
+  if (request.readyState === 4 && request.status < 400){
+    return callback(JSON.parse(request.responseText).tracks.items);
+    }
+  }
+  request.open("GET", 'https://api.spotify.com/v1/search?q='+ opt +'&type=track');
+  request.send();
+}
+
+function albumExistsAlready(list, imageUrl) {
+  return list.indexOf(imageUrl) == -1
+}
+
+function start(){
   scroll.innerHTML = "";
   var list = [];
   var check = [];
@@ -15,47 +30,43 @@ search.addEventListener("click", function start(){
   if(opt === ""){
     opt = randomword;
   }
-  var request = new XMLHttpRequest();
-  request.onreadystatechange = function(){
-    if (request.readyState === 4 && request.status < 400){
-      var array = JSON.parse(request.responseText).tracks.items;
-      for (var album in array){
-        var element = array[album].album.images[1].url;
-        if (list.indexOf(element) == -1){
-        list.push(element);
-        }
-        for (var el in list){
-          if (check.indexOf(list[el]) == -1){
-          var img = document.createElement("img");
-          img.src = list[el];
-          check.push(list[el]);
-          if (list[el] === array[album].album.images[1].url){
-            img.setAttribute("id", array[album].album.id);
+
+  getAlbums(opt, function (albums) {
+    for (var album in albums){
+      var imageUrl = albums[album].album.images[1].url;
+      list.push({
+        url: imageUrl,
+        id: albums[album].album.id
+      })
+      for (var i in list){
+        var img = document.createElement("img");
+        img.src = list[i].url;
+        img.id = list[i].id;
+      }
+      scroll.appendChild(img);
+      scroll.style.width = list.length*100 + "px";
+    }
+  });
+
+    var images = document.getElementsByTagName("img");
+    for (var i = 0; i < images.length; i++) {
+      images[i].addEventListener("click", function(event){
+          var target = event.target;
+          for (var al in array){
+            if ((array[al].album.id == target.id) && (collection.indexOf(array[al].album.name) == -1)){
+              var newl = document.createElement("h5");
+              newl.innerHTML = array[al].artists[0].name+": "+ array[al].album.name;
+              select.appendChild(newl);
+              collection.push(array[al].album.name);
             }
           }
-        }
-        scroll.appendChild(img);
-        scroll.style.width = check.length*100 + "px";
-      }
-      var images = document.getElementsByTagName("img");
-      for (var i = 0; i < images.length; i++) {
-        images[i].addEventListener("click", function(event){
-            var target = event.target;
-            for (var al in array){
-              if ((array[al].album.id == target.id) && (collection.indexOf(array[al].album.name) == -1)){
-                var newl = document.createElement("h5");
-                newl.innerHTML = array[al].artists[0].name+": "+ array[al].album.name;
-                select.appendChild(newl);
-                collection.push(array[al].album.name);
-              }
-            }
-        });
-      }
+      });
     }
-  };
-  request.open("GET", 'https://api.spotify.com/v1/search?q='+ opt +'&type=track');
-  request.send();
-});
+}
+
+start();
+
+search.addEventListener("click", start);
 
 clear.addEventListener("click", function(){
   select.innerHTML = "";
@@ -64,11 +75,11 @@ clear.addEventListener("click", function(){
 
 submit.addEventListener("click", function(){
   var request = new XMLHttpRequest();
+  request.open("POST", "https://lit-fortress-6467.herokuapp.com/post");
+  request.send(collection);
   request.onreadystatechange = function(){
     if (request.readyState === 4 && request.status < 400){
         console.log(request.responseText);
     }
   };
-    request.open("POST", "https://lit-fortress-6467.herokuapp.com/post");
-    request.send(collection);
 });
